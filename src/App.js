04b6af6688d25bddb284ac1aab22d169e5d3f64a
Jonathan Forrider <jd4rider@gmail.com>
2018-10-22 22:17:06 -0600

    final commit

--- src/App.js
+++ src/App.js
@@ -4,33 +4,15 @@ import './App.css';
 import FindFlight from './components/FindFlight';
 
 class App extends Component {
-  componentDidMount = () => {
-    fetch("https://skyscanner-skyscanner-flight-search-v1.p.mashape.com/apiservices/autosuggest/v1.0/US/USD/en-US/?query=Missoula", {
-      method: "GET",
-      headers: {
-        "X-Mashape-Key": process.env.REACT_APP_MASHAPE_API_KEY,
-        "X-Mashape-Host": "skyscanner-skyscanner-flight-search-v1.p.mashape.com"
-      }
-    }).then(function (response) {
-      //response.status     //=> number 100–599
-      //response.statusText //=> String
-      //response.headers    //=> Headers
-      //response.url        //=> String
-
-      //return response.text()
-      console.log(response.text());
-    }, function (error) {
-      //error.message //=> String
-    })
-  }
-
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+      <div>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to Flights</h1>
+          </header>
+        </div>
         <FindFlight></FindFlight>
       </div>
     );
--- src/components/FindFlight.js
+++ src/components/FindFlight.js
@@ -1,25 +1,119 @@
 import React, { Component } from 'react';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import FlightCards from './FlightCards';
 
 export default class FindFlight extends Component {
+  constructor(props){
+    super(props);
+
+    this.getFormattedDate = (date) => {
+      var year = date.getFullYear();
+
+      var month = (1 + date.getMonth()).toString();
+      month = month.length > 1 ? month : '0' + month;
+
+      var day = (1 + date.getDate()).toString();
+      day = day.length > 1 ? day : '0' + day;
+
+      return year + '-' + month + '-' + day;
+    }
+
+    this.getFlightInfo = (event) => {
+      event.preventDefault();
+      this.findAirport(this.state.fromcity, 'fromairport', this);
+      this.findAirport(this.state.tocity, 'toairport', this);
+
+    }
+
+    this.findAirport = (city, key) => {
+      fetch("https://skyscanner-skyscanner-flight-search-v1.p.mashape.com/apiservices/autosuggest/v1.0/US/USD/en-US/?query="+city, {
+        method: "GET",
+        headers: {
+          "X-Mashape-Key": process.env.REACT_APP_MASHAPE_API_KEY,
+          "X-Mashape-Host": "skyscanner-skyscanner-flight-search-v1.p.mashape.com"
+        }
+      }).then(response => {
+        return response.json();
+      }, error =>  {
+      }).then(jsonData => {
+        this.setState({ [key]: jsonData.Places[0].PlaceId });
+        console.log(this.state);
+        if (this.state.toairport.length > 0 && this.state.fromairport.length > 0){
+          this.findFlight();
+        }
+      })
+    }
+
+    this.findFlight = () => {
+      let datefrom;
+      let dateto;
+      if(this.state.fromdate === '') datefrom = 'anytime';
+      else {
+        let df = new Date(this.state.fromdate);
+        datefrom = this.getFormattedDate(df);
+      }
+      if(this.state.todate === '') dateto = 'anytime';
+      else {
+        let dt = new Date(this.state.todate);
+        dateto = this.getFormattedDate(dt);
+      }
+      console.log('Date From: '+datefrom);
+      console.log('Date To: '+dateto);
+
+      fetch("https://skyscanner-skyscanner-flight-search-v1.p.mashape.com/apiservices/browsedates/v1.0/US/USD/en-US/" + this.state.fromairport + "/" + this.state.toairport + "/" + datefrom + "/" + dateto, {
+        method: "GET",
+        headers: {
+          "X-Mashape-Key": process.env.REACT_APP_MASHAPE_API_KEY,
+          "X-Mashape-Host": "skyscanner-skyscanner-flight-search-v1.p.mashape.com"
+        }
+      }).then(response => {
+        return response.json();
+      }, error => {
+      }).then(jsonData => {
+        console.log(jsonData);
+        if (jsonData.ValidationErrors != null) this.setState({ flights: [] });
+        else this.setState({ flights: jsonData.Quotes });
+      })
+    }
+
+    this.handleOnchange = (event) => {
+      this.setState({[event.target.id]: event.target.value });
+    }
+
+    this.state={
+      fromcity: '',
+      tocity:'',
+      fromdate: '',
+      todate: '',
+      fromairport: '',
+      toairport: '',
+      flights: [],
+    }
+  }
   render() {
     return (
       <div className='container'>
-        <Form inline>
-          <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-            <Label for="fromcity" className="mr-sm-2">From City</Label>
-            <Input type="text" name="fromcity" id="fromcity" placeholder="City Flying From" />
+        <Form>
+          <FormGroup>
+            <Label for="fromcity">From City</Label>
+            <Input type="text" name="fromcity" id="fromcity" placeholder="City Flying From" onChange={this.handleOnchange} />
           </FormGroup>
-          <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-            <Label for="tocity" className="mr-sm-2">To City</Label>
-            <Input type="text" name="tocity" id="tocity" placeholder="City Flying To" />
+          <FormGroup>
+            <Label for="tocity">To City</Label>
+            <Input type="text" name="tocity" id="tocity" placeholder="City Flying To" onChange={this.handleOnchange} />
           </FormGroup>
-          <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-            <Label for="flightdate" className="mr-sm-2">Password</Label>
-            <Input type="date" name="flightdate" id="flightdate" placeholder="don't tell!" />
+          <FormGroup>
+            <Label for="fromdate">From Date (Leave Blank for Anytime)</Label>
+            <Input type="date" name="fromdate" id="fromdate" onChange={this.handleOnchange} />
           </FormGroup>
-          <Button>Check Availability</Button>
+          <FormGroup>
+            <Label for="todate">To Date (Leave Blank for Anytime)</Label>
+            <Input type="date" name="todate" id="todate" onChange={this.handleOnchange} />
+          </FormGroup>
+          <Button onClick={this.getFlightInfo}>Check Availability</Button>
         </Form>
+        <br />
+        { this.state.flights.length > 1 ? <FlightCards concstate={this.state} /> : null }
       </div>
     );
   }
